204cfaa204a69a613fc43313b94423bb9a050109
julianodepaula <julianodepaula@gmail.com>
2021-12-09 12:50:53 -0300

    Criando os primeiros serviços com TDD // REST API #3

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -1,13 +1,26 @@
+const crypto = require('crypto');
 const axios = require('axios');
+const postsService = require('../service/postsService');
+
+const generate = function(){
+    return crypto.randomBytes(20).toString('hex');
+};
 
 test('Should get posts', async function () {
+// given - dado que
+    const post1 = await postsService.savePost({ title: generate(), content: generate() });
+    const post2 = await postsService.savePost({ title: generate(), content: generate() });
+    const post3 = await postsService.savePost({ title: generate(), content: generate() });
+
+// when - quando acontecer    
     const response = await axios({
         url: 'http://localhost:3000/posts',
         method: 'get'
     });
     const posts = response.data;
-    expect(posts).toHaveLength(3);
-    const [firstPost] = posts;
-    expect(firstPost.id).toBe(1);
-    expect(firstPost.title).toBe('REST API: Métodos');
+// then
+   expect(posts).toHaveLength(3);
+   await postsService.deletePost(post1.id);
+   await postsService.deletePost(post2.id);
+   await postsService.deletePost(post3.id);
 });
